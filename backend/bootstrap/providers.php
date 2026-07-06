45e9af9b61c496dc125dddf74bc3e135316d13d6
Radouane EL ASRI <54957690+radouane99@users.noreply.github.com>
2026-07-06 09:57:06 +0100

    feat: initialize backend project with Laravel framework, Horizon, Telescope, and Reverb support

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
     App\Providers\AiServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
--- backend/routes/console.php
+++ backend/routes/console.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('backup:clean')->daily();
+Schedule::command('backup:run')->daily();
+
