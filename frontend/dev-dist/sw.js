2f377299d65d18256c784c23dc78c60bf0f06660
Radouane EL ASRI <54957690+radouane99@users.noreply.github.com>
2026-07-16 12:13:09 +0100

    feat: add service worker configuration for offline caching and navigation routing

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.khbddk4h1i"
+    "revision": "0.hmtl4gm1ptg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
