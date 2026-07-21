c245753a2555aee15727332e5ffc7e29f8daf0da
Radouane EL ASRI <54957690+radouane99@users.noreply.github.com>
2026-07-21 16:44:41 +0200

    feat: initialize service worker with Workbox precaching and navigation routing

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.q8t5qhlldlo"
+    "revision": "0.r1kjqpddo9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
